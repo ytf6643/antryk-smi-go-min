2b3fe52ce8a92a37ba17fdba9a57c11517a91d4c
ytf6643 <ytf6643@gmail.com>
2026-06-26 16:01:23 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,30 +1,29 @@
 package main
 
 import (
-   "net/http"
-   "os"
-   "os/exec"
-  )
+    "fmt"
+    "net/http"
+    "os"
+    "os/exec"
+   )
 
 func main() {
-   http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-       w.Write([]byte("ok
-                      "))
-                       })
-                http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-                    b, e := exec.Command("nvidia-smi").CombinedOutput()
-                    w.Write([]byte("CUDA_VISIBLE_DEVICES=" + os.Getenv("CUDA_VISIBLE_DEVICES") + "
-                                   "))
-                                     if e != nil {
-                                          w.Write([]byte(e.Error() + "
-                                                         "))
-                                                           }
-                                                           w.Write(b)
-                                                          })
-                                                   p := os.Getenv("PORT")
-                                                   if p == "" {
-                                                       p = "8000"
-                                                      }
-                                                   http.ListenAndServe(":"+p, nil)
-                                                  }
-                                                  
+    http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+         fmt.Fprintln(w, "ok")
+        })
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+         out, err := exec.Command("nvidia-smi").CombinedOutput()
+         fmt.Fprintln(w, "CUDA_VISIBLE_DEVICES="+os.Getenv("CUDA_VISIBLE_DEVICES"))
+         if err != nil {
+               fmt.Fprintln(w, err.Error())
+              }
+         w.Write(out)
+        })
+
+    port := os.Getenv("PORT")
+    if port == "" {
+         port = "8000"
+        }
+    http.ListenAndServe(":"+port, nil)
+   }
